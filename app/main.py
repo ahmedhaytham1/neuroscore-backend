0686a72ee02ebbb1aae54d2bc685faa7346e8dbc
Ahmed Haytham <ahmedhaytham2209@gmail.com>
2026-05-17 03:19:48 +0300

    fix cors

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ app = FastAPI(title="Parkinson Video AI Backend", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
